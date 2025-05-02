5b1e6fa91b30fb38210bb9843444a66af460d6ed
Mingu Kang <mingu2001@gmail.com>
2025-05-02 17:57:48 +0900

    Update version to 1.0.5 in WidgetServerBase and AssemblyInfo

--- WidgetServerBase.cs
+++ WidgetServerBase.cs
@@ -35,7 +35,7 @@ namespace AudioVisualizerWidget {
         }
         public Version Version {
             get {
-                return new Version(1,0,4);
+                return new Version(1,0,5);
             }
         }
 
